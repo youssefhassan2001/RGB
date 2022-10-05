aca44c875e520f33e4c9bf2c753071dec2165893
youssefhassan2001 <youssefhassan2001@gmail.com>
2022-10-05 04:07:41 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,49 @@
+$('.face').on('click', function () {
+
+    location.href ='https://www.facebook.com/'
+});
+$('.insta').on('click', function () {
+
+    location.href ='https://www.instagram.com/'
+});
+$('.google').on('click', function () {
+
+    location.href ='https://ads.google.com/intl/ar_EG/home/'
+});$('.Youtube-Advertisements').on('click', function () {
+
+    location.href ='https://ads.google.com/intl/ar_EG/home/'
+});$('tweter').on('click', function () {
+
+    location.href ='https://ads.twitter.com/login'
+});$('.snap').on('click', function () {
+
+    location.href ='https://ads.snapchat.com/'
+});$('.form').on('click', function () {
+
+    location.href ='https://www.google.com/forms/about/'
+});$('.sheet').on('click', function () {
+
+    location.href ='https://www.google.com/sheets/about/'
+});$('.can').on('click', function () {
+
+    location.href ='https://www.canva.com/'
+});$('.karma').on('click', function () {
+
+    location.href ='https://www.fanpagekarma.com/'
+});$('.go').on('click', function () {
+
+    location.href ='https://trends.google.com/trends/?geo=EG'
+});
+$('.hot').on('click', function () {
+
+    location.href ='https://www.hootsuite.com/'
+});
+$('.right').on('click', function () {
+
+    location.href ='https://www.rightrelevance.com/'
+});
+
+$('.ana').on('click', function () {
+
+    location.href ='https://analytics.google.com/analytics/web/provision/#/provision'
+});
